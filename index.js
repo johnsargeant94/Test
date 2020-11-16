4e4ec7b26cc53d24c921df0d2bfdde897641adca
John Sargeant <71139230+johnsargeant94@users.noreply.github.com>
2020-11-16 15:19:45 +0000

    Added connection to MongoDB

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 require('dotenv').config();
 
 const app = express();
+const url = process.env.MongoDB_URL
+mongoose.connect(url, {
+    useNewUrlParser: true,
+    useFindAndModify:false,
+    useUnifiedTopology: true
+});
 
 app.get('/', (req, res) => {
     res.send('Hello Heroku!');
